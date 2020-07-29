a32ab048345cffa6341d2db9748da1e9432ffa08
kadu maverick <kaduhermanni@gmail.com>
2020-07-28 21:38:38 -0300

    add case update and spread of virus

---
+++ lib/main.dart
@@ -0,0 +1,281 @@
+import 'package:covid_19_app/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Covid 19',
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        fontFamily: "Poppins",
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: kBodyTextColor),
+        ),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            ClipPath(
+              clipper: MyClipper(),
+              child: Container(
+                padding: EdgeInsets.only(left: 40, top: 50, right: 20),
+                height: 350,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Color(0xFF3383CD),
+                      Color(0xFF11249f),
+                    ],
+                  ),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/virus.png'),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Align(
+                        alignment: Alignment.topRight,
+                        child: SvgPicture.asset('assets/icons/menu.svg')),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Expanded(
+                      child: Stack(
+                        children: <Widget>[
+                          SvgPicture.asset(
+                            'assets/icons/Drcorona.svg',
+                            width: 210,
+                            fit: BoxFit.fitWidth,
+                            alignment: Alignment.topCenter,
+                          ),
+                          Positioned(
+                            top: 20,
+                            left: 125,
+                            child: Text(
+                              'All you need \nis stay at home.',
+                              style: kHeadingTextStyle.copyWith(
+                                  color: Colors.white),
+                            ),
+                          ),
+                          Container(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              height: 60,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25),
+                border: Border.all(
+                  color: Color(0xffe5e5e5),
+                ),
+              ),
+              child: Row(
+                children: <Widget>[
+                  SvgPicture.asset('assets/icons/maps-and-flags.svg'),
+                  SizedBox(
+                    width: 60,
+                  ),
+                  Expanded(
+                      child: DropdownButton(
+                          isExpanded: true,
+                          underline: SizedBox(),
+                          icon: SvgPicture.asset('assets/icons/dropdown.svg'),
+                          items: ['indonesia', 'bangladesh', 'United States']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (value) {}))
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: 'Case Update\n',
+                              style: kTitleTextstyle,
+                            ),
+                            TextSpan(
+                              text: 'Newest update March 20',
+                              style: TextStyle(color: kTextLightColor),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        'See Details',
+                        style: TextStyle(
+                            color: kPrimaryColor, fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            offset: Offset(0, 4),
+                            blurRadius: 30,
+                            color: kShadowColor)
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Counter(
+                          title: 'Infected',
+                          number: 1405,
+                          color: kInfectedColor,
+                        ),
+                        Counter(
+                          title: 'Deaths',
+                          number: 87,
+                          color: kDeathColor,
+                        ),
+                        Counter(
+                          title: 'Recovered',
+                          number: 46,
+                          color: kRecovercolor,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'Spread of Virus',
+                        style: kTitleTextstyle,
+                      ),
+                      Text(
+                        'See details',
+                        style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Counter extends StatelessWidget {
+  const Counter({
+    Key key,
+    this.number,
+    this.color,
+    this.title,
+  }) : super(key: key);
+
+  final int number;
+  final Color color;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(6),
+          height: 25,
+          width: 25,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color.withOpacity(.26),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.transparent,
+              border: Border.all(color: color, width: 2),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          '$number',
+          style: TextStyle(fontSize: 40, color: color),
+        ),
+        Text(
+          title,
+          style: kSubTextStyle,
+        )
+      ],
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
